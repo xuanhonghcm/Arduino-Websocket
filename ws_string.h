af3754a5342c72946f5382430d2e6b3dd9c54327
Ahti Legonkov <ahti.legonkov@gmail.com>
2013-02-13 18:10:38 +0200

    Revert "ws_string for platform ASF"
    This reverts commit 7483c2bc1e5769ddbc485bd438d1575cb3ab5ae5.

--- ws_string.h
+++ ws_string.h
@@ -2,8 +2,6 @@
 
 #if defined(ARDUINO)
 	#include "platform/arduino/ws_string.h"
-#elif defined(ASF)
-    #include "platform/ASF/ws_string.h"
 #else
 	#include "platform/generic/ws_string.h"
 #endif
