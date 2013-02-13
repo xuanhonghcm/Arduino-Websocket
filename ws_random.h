eaf18ee9e3d53398659c853a762f15f900771320
Ahti Legonkov <ahti.legonkov@gmail.com>
2013-02-13 18:20:31 +0200

    Allow users to provide their own implementation of random.

--- ws_random.h
+++ ws_random.h
@@ -1,9 +1,19 @@
 #pragma once
 
-#if defined(ARDUINO)
-	#include "platform/arduino/ws_random.h"
-#elif defined(ASF)
-    #include "platform/ASF/ws_random.h"
-#else
-	#include "platform/generic/ws_random.h"
+#if defined(HAVE_LIBWEBSOCKET_RANDOM)
+    namespace websocket
+    {
+        void randomSeed();
+        uint32_t random();
+    }
+#endif
+
+#if !defined(HAVE_LIBWEBSOCKET_RANDOM) && defined(ARDUINO)
+    #define HAVE_LIBWEBSOCKET_RANDOM
+    #include "platform/arduino/ws_random.h"
+#endif
+
+#if !defined(!defined(HAVE_LIBWEBSOCKET_RANDOM))
+    #define HAVE_LIBWEBSOCKET_RANDOM
+    #include "platform/generic/ws_random.h"
 #endif
